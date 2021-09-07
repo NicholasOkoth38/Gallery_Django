7ee75c456bab70c44e2996747a15dd426d1d81f1
NicholasOkoth38 <nicholas.okoth@student.moringaschool.com>
2021-09-07 14:24:28 +0300

    redeploying

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -203,49 +203,24 @@ USE_TZ=True
 # # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL='/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS=(
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
-
-# this is from github deployment
-
-BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL='/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static'),
-
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-
-    STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    # configuring the location for media
-    MEDIA_URL='/media/'
-    MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-
-    # Configure Django App for Heroku.
-    django_heroku.settings(locals())
